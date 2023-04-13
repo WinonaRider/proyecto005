50fc9e6b4989acf73f243f231e1b9f2f6c6d7df7
unknown <marchioripedro1@gmail.com>
2023-04-12 21:10:59 -0300

    update all files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+
+  function eliminarUltimaFila() {
+    if (articulos.length > 0) {
+      const temp=Array.from(articulos)
+      temp.pop()
+      setArticulos(temp)
+    }
+  }
+
+  const [articulos, setArticulos] = useState([{
+    codigo: 1,
+    descripcion: 'papas',
+    precio: 12.52
+  }, {
+    codigo: 2,
+    descripcion: 'naranjas',
+    precio: 21
+  }, {
+    codigo: 3,
+    descripcion: 'peras',
+    precio: 18.20
+  }]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <table border="1">
+        <thead><tr><th>Código</th><th>Descripción</th><th>Precio</th></tr></thead>
+        <tbody>
+          {articulos.map(art => {
+            return (
+              <tr key={art.codigo}>
+                <td>
+                  {art.codigo}
+                </td>
+                <td>
+                  {art.descripcion}
+                </td>
+                <td>
+                  {art.precio}
+                </td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+      <button onClick={eliminarUltimaFila}>Eliminar última fila</button>
     </div>
   );
 }
